8cbaeb529ac1f1af0e86724b77efec762ca41d3d
Roberto Armijo Keller <rak.scl@gmail.com>
2020-07-08 23:06:43 -0400

    update

--- Google Analytics prophet/prophet.R
+++ Google Analytics prophet/prophet.R
@@ -1,4 +1,3 @@
-setwd("~/Dropbox/Ripley/Analytics/prophet")
 options("scipen" = 10)
 options()$scipen
 library(ggplot2)
@@ -8,34 +7,30 @@ library(lubridate)
 library(prophet)
 library(dplyr)
 
-# 
-# load(file="~/Dropbox/ripley/Analytics/GA/oauth_token")
-# ValidateToken(oauth_token)
-# query.list <- Init(start.date = as.Date("2014-01-01"),
-#                    end.date = as.Date(Sys.Date()),
-#                    dimensions = "ga:date",
-#                    metrics = "ga:sessions,ga:itemRevenue",
-#                    max.results = 1000,
-#                    sort = "ga:date",
-#                    #filters ="ga:sourceMedium==mail",
-#                    table.id = "ga:19886580")
-# ga.query <- QueryBuilder(query.list)
-# ga.data <- GetReportData(ga.query, oauth_token, split_daywise = T)
-# ga.data$date <- as.Date(ga.data$date, "%Y%m%d")
-# ga.data$day <- wday(ga.data$date,label = TRUE)
-# save(ga.data,file="gadata.rda")
-load(file="gadata.rda")
+
+load(file="oauth_token")
+ValidateToken(oauth_token)
+query.list <- Init(start.date = as.Date("2014-01-01"),
+                   end.date = as.Date(Sys.Date()),
+                   dimensions = "ga:date",
+                   metrics = "ga:sessions,ga:itemRevenue",
+                   max.results = 1000,
+                   sort = "ga:date",
+                   filters ="ga:sourceMedium==mail",
+                   table.id = "ga:19886580")
+ga.query <- QueryBuilder(query.list)
+ga.data <- GetReportData(ga.query, oauth_token, split_daywise = T)
+ga.data$date <- as.Date(ga.data$date, "%Y%m%d")
+ga.data$day <- wday(ga.data$date,label = TRUE)
 stats <- subset(ga.data,select=c("date","itemRevenue"))
 colnames(stats) <- c("ds", "y")
 
-#stats <- subset(ga.data,select=c("date","itemRevenue","sessions"))
-#colnames(stats) <- c("ds", "x","y")
-
+stats <- subset(ga.data,select=c("date","itemRevenue","sessions"))
+colnames(stats) <- c("ds", "x","y")
 head(stats)
 
 View(summary(stats))
 plot(y ~ ds, stats, type = "l")
-
 m <- prophet(stats)
 future <- make_future_dataframe(m, periods = 130)
 forecast <- predict(m, future)
@@ -51,14 +46,16 @@ cyber <- data_frame(
   lower_window = 0,
   upper_window = 4
 )
-#navidad <- data_frame(
-#  holiday = "Cyber",
-#  ds = as.Date(c("2017-12-01","2016-12-01","2015-12-01", "2014-12-01", "2015-11-16", "2015-05-24", "2014-11-16", "2014-07-28")),
-#  lower_window = 0,
-#  upper_window = 20
-#)
+navidad <- data_frame(
+ holiday = "Cyber",
+ ds = as.Date(c("2017-12-01","2016-12-01","2015-12-01", "2014-12-01", "2015-11-16", "2015-05-24", "2014-11-16", "2014-07-28")),
+ lower_window = 0,
+ upper_window = 20
+)
+
 fechas <- bind_rows(cyber,navidad)
-m <- prophet(stats, holidays = cyber,holidays.prior.scale = 15)
+m <- prophet(stats, holidays = fechas,holidays.prior.scale = 15)
+
 forecast <- predict(m, future)
 plot(m, forecast)
 
